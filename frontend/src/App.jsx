e37cd71c9b335dd506670f9d22ca41248d0e5ae5
arpitjaswal <iamarpitjaswal@gmail.com>
2023-09-17 01:10:19 +0530

    feat: initiated backend, dummy api created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <center>
+      <h1>Accommodation chahiye, aajao dikha dunga!</h1>
+     
+      </center>
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <>
+        const data = fetch("http://localhost:3000/listings")
+        data.then()
+    </>
+  )
+}
+
+export default Card
